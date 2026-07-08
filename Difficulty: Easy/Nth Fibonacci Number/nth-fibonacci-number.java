d32170d26a030892fc2cf0b79c03d20079bcdf60
Krishna Asati <krishnaasati2006@gmail.com>
2026-07-08 16:31:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.java
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.java
@@ -1,13 +1,25 @@
 // code using recursion 
 
-class Solution{
+// class Solution{
+//         public int fibo(int n){
+//             // code here
+//             if(n<=1) return n;
+//             return fibo(n-1) + fibo(n-2);
+//         }
+//         public int nthFibonacci(int n){
+//             return fibo(n);
+//         }
+        
+//     }
+
+ class Solution{
         public int fibo(int n){
-            // code here
             if(n<=1) return n;
-            return fibo(n-1) + fibo(n-2);
+            int ans = fibo(n-1) + fibo(n-2);
+            return ans;
         }
         public int nthFibonacci(int n){
             return fibo(n);
         }
-        
+
     }
