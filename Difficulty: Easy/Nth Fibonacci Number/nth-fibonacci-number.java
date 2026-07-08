6f1cb83849e0b98ac3a0d866521fb6b7f02eb411
Krishna Asati <krishnaasati2006@gmail.com>
2026-07-08 17:45:55 +0530

    Optimize nth Fibonacci calculation with dynamic programming
    Refactored Fibonacci calculation to use dynamic programming for improved efficiency.

--- Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.java
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.java
@@ -1,4 +1,4 @@
-// code using recursion 
+// code using recursion----------------------------->
 
 // class Solution{
 //         public int fibo(int n){
@@ -12,13 +12,32 @@
         
 //     }
 
+//  class Solution{
+//         public int fibo(int n){
+//             if(n<=1) return n;
+//             int ans = fibo(n-1) + fibo(n-2);
+//             return ans;
+//         }
+//         public int nthFibonacci(int n){
+//             return fibo(n);
+//         }
+
+//     }
+
+
+// USING DYNAMINC PROGRAMING ------------------>
+
  class Solution{
+        static int [] dp;
         public int fibo(int n){
             if(n<=1) return n;
+            if(dp[n]!=0) return dp[n];  // extra
             int ans = fibo(n-1) + fibo(n-2);
+            dp[n] = ans;  // extra 
             return ans;
         }
         public int nthFibonacci(int n){
+            dp = new int[n+1];
             return fibo(n);
         }
 
