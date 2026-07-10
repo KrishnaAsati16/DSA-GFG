2ddf082e91388e123eb214e117a621fff394fe25
Krishna Asati <krishnaasati2006@gmail.com>
2026-07-10 14:51:02 +0530

    Update count-paths-from-top-left-to-bottom-right.java

--- Difficulty: Medium/Count Paths from Top Left to Bottom Right/count-paths-from-top-left-to-bottom-right.java
+++ Difficulty: Medium/Count Paths from Top Left to Bottom Right/count-paths-from-top-left-to-bottom-right.java
@@ -10,16 +10,33 @@
 //     }
 // }
 
+// class Solution {
+//     public int numberOfPaths(int m, int n) {
+//         int N = m + n - 2;
+//         int r = m - 1;
+        
+//         long res = 1;
+//         for (int i = 1; i <= r; i++) {
+//             res = res * (N - r + i) / i;
+//         }
+        
+//         return (int) res;
+//     }
+// }
+
+
+// using Dynamic programming--------------------->
+
 class Solution {
-    public int numberOfPaths(int m, int n) {
-        int N = m + n - 2;
-        int r = m - 1;
-        
-        long res = 1;
-        for (int i = 1; i <= r; i++) {
-            res = res * (N - r + i) / i;
-        }
-        
-        return (int) res;
-    }
+     static int[][] dp;
+     public int numberOfPaths(int m, int n) {
+         dp = new int [m+1][n+1];   // rows -> 0 to m, cols-> 0 to n
+         return paths(m,n);
+     }
+     public int paths(int m, int n){
+         if(m==1 || n==1) return 1;
+         if(dp[m][n]!=0) return dp[m][n];
+         return dp[m][n] = paths(m-1,n) + paths(m,n-1);
+     }
 }
+
