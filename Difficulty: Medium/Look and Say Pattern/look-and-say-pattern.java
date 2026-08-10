210c6954774adc3ee3cf02b773310a67f6a3292a
Krishna Asati <krishnaasati2006@gmail.com>
2026-08-10 21:53:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Look and Say Pattern/look-and-say-pattern.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String countAndSay(int n) {
+        // code here
+        if(n==1) return "1";
+        String s = countAndSay(n-1) + "a";
+        StringBuilder ans = new StringBuilder();
+        int i = 0, j = 0;
+        while(j<s.length()){
+            if(s.charAt(i)==s.charAt(j)) j++;
+            else{
+                int count = j-i;
+                ans.append(count);
+                ans.append(s.charAt(i));
+                i = j;
+            }
+        }
+        return ans.toString();
+    }
+}
