59246a5c4d784900e7af4559602be308fa6adce4
Krishna Asati <krishnaasati2006@gmail.com>
2026-08-15 22:38:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Prime Number/prime-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    static boolean isPrime(int n) {
+        // code here
+        if (n <= 1) return false;
+            if (n == 2 || n == 3) return true;
+
+            // Eliminate even numbers
+            if (n % 2 == 0) return false;
+
+            // Check divisibility up to √n
+            for (int i = 3; i <= Math.sqrt(n); i += 2) {
+                if (n % i == 0) return false;
+            }
+            return true;
+        
+    }
+}
